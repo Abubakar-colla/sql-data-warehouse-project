faf3574625b548dffff89e9c96b1e0bb6f6cc447
Abubakar Suleiman <suleiman.ab@northeastern.edu>
2025-03-05 20:35:23 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,74 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+===============================================================================
+*/
+
+DELIMITER $$
+
+CREATE PROCEDURE BRONZE_STAGE()
+BEGIN
+    TRUNCATE TABLE bronze.crm_cust_info;
+    TRUNCATE TABLE bronze.crm_prd_info;
+    TRUNCATE TABLE bronze.crm_sales_details;
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+END $$
+
+DELIMITER ;
+
+CALL BRONZE_STAGE();
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/cust_info.csv'
+INTO TABLE  bronze.crm_cust_info
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/prd_info.csv'
+INTO TABLE  bronze.crm_prd_info
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_details.csv'
+INTO TABLE  bronze.crm_sales_details
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+ 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/cust_az12.csv'
+INTO TABLE  bronze.erp_cust_az12
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/loc_a101.csv'
+INTO TABLE  bronze.erp_loc_a101
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/px_cat_g1v2.csv'
+INTO TABLE  bronze.erp_px_cat_g1v2
+FIELDS TERMINATED BY ','  
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
